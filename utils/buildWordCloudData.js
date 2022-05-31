05be28f46ae690ce6b2183d5109ca9f8fbef27e9
Lisa Martin <lisamartin00@gmail.com>
2022-05-31 11:46:43 -0600

    bail out on reserved words before adding them to object, add more words to blacklist

--- utils/buildWordCloudData.js
+++ utils/buildWordCloudData.js
@@ -1,5 +1,5 @@
 const blacklist = [
-  'of', 'to', 'is', 'in', 'on', 'it\'s', 'and', 'the', 'isnt', 'how', 'with', 'has', 'after', 'he', 'she', 'a', 'an', 'about', 'your', 'isnt', 'its', 'this', 'what', 'i', 'have', 'one', 'two', 'had', 'then', 'it', 'from', 'that', 'should', 'when', 'was', 'than', 'internalstorylineno', 'some', 'at', 'more', 'where', 'we', 'for', 'you', 'quiz', 'my', 'other', 'didnt', 'internal-sub-only-nl', 'be', 'are', 'not', 'gets', 'audio-neutral-informative', 'by', 'his', 'her', 'could', 'can', 'them', 'do',
+  'of', 'to', 'is', 'in', 'on', 'it\'s', 'and', 'the', 'isnt', 'how', 'with', 'has', 'after', 'he', 'she', 'a', 'an', 'about', 'your', 'isnt', 'its', 'this', 'what', 'i', 'have', 'one', 'two', 'had', 'then', 'it', 'from', 'that', 'should', 'when', 'was', 'than', 'internalstorylineno', 'some', 'at', 'more', 'where', 'we', 'for', 'you', 'quiz', 'my', 'other', 'didnt', 'internal-sub-only-nl', 'be', 'are', 'not', 'gets', 'audio-neutral-informative', 'by', 'his', 'her', 'could', 'can', 'them', 'do', 'as', 'but', 'they', 'their', 'did', 'why', 'really'
 ];
 
 const buildWordCloudData = (stories, topStories,) => {
@@ -9,21 +9,19 @@ const buildWordCloudData = (stories, topStories,) => {
   stories?.forEach((story, index,) => {
     story?.forEach((word,) => {
       const wordKey = word.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=_`~?’—‘]|(content type)|(\(.*\))/g, '',);
-      if (blacklist.includes(wordKey)) return; // bail out here
       const count = wordList[wordKey];
       const isReservedWord = typeof count === 'function';
+      if (blacklist.includes(wordKey) || isReservedWord) return; // bail out here
       const articles = articleList?.[wordKey];
       const storyArticle = topStories?.[index];
       // A word can appear more than once in the same headline
       const isDuplicate = articles?.filter((article => article.headline === storyArticle.title)).length > 0;
-      if (!isReservedWord && !isDuplicate) {
+      if (!isDuplicate) {
         articleList[wordKey] = [
           ...(articles ?? []),
           { headline: storyArticle.title, url: storyArticle.url, },
         ];
         wordList[wordKey] = count === undefined ? 1 : count + 1;
-      } else {
-        console.log(`reserved word found: "${wordKey}"`,);
       }
     },);
   },);
